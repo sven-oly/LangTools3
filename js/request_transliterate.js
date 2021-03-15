2e7ee47a6dad69a58cd59669bf2fdb26c8c28c76
Craig Cornelius <cwcornelius@gmail.com>
2021-03-15 12:09:47 -0700

    Fix Burmese Qwerty layout. Add download/upload modified rules

--- js/request_transliterate.js
+++ js/request_transliterate.js
@@ -1,6 +1,6 @@
 // Javascript to request XHTML and receive results.
 
-function requestTransliterate(inputarea, outputarea, rulearea, messagearea, summaryarea, translit_selection) {
+function requestTransliterate(inputarea, outputarea, rule_text, messagearea, summaryarea, translit_selection) {
     // Prepare for the call to the backendvar xmlhttp;
     if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
       xmlhttp=new XMLHttpRequest();
@@ -41,11 +41,10 @@ function requestTransliterate(inputarea, outputarea, rulearea, messagearea, summ
     const inputObj = document.getElementById(inputarea);
     const inputtext = escape(inputObj.value);
     const encodedInput = encodeURIComponent(inputObj.value);
-    const ruleObj = document.getElementById(rulearea);
-    const ruletext = encodeURIComponent(ruleObj.value);
+    const rules_to_send = encodeURIComponent(rule_text);
     const translit_type = translit_selection;
 
-    const args = "input=" + encodedInput + "&rules=" + ruletext +
+    const args = "input=" + encodedInput + "&rules=" + rules_to_send +
                      "&translit_type=" + translit_type;
     const url_head = "/my/dotranslit/";
     // var target = "/my/dotranslit/" + args;
@@ -79,6 +78,7 @@ function requestTransliterateRules(rulearea, messagearea, translit_selection) {
         }
         messageObj.value = messageObj.innerHTML = json_obj.message;
         rulearea_obj.value = rulearea_obj.innerHTML = json_obj.rulesText;
+        prepareRuleEditZones(json_obj.rulesText, "editDiv");
       }
     }
 
@@ -89,4 +89,50 @@ function requestTransliterateRules(rulearea, messagearea, translit_selection) {
     xmlhttp.open("GET", target, true);
     var size = target.length;
     xmlhttp.send(null);
+}
+
+// Create a separate text are for each phase of rule list
+function prepareRuleEditZones(intext, div_id) {
+    var phases = intext.split("::Null;");
+    var div = document.getElementById(div_id);
+    // Clear existing items in this div.
+    var areas = div.getElementsByTagName('textarea');
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+
+    for (phase in phases) {
+        // To use scoped valuesi in the closure
+      let input = document.createElement("textarea");
+      let button = document.createElement("button");
+      // put parts of the text in each
+      input.name = "Phase " + phase;
+      input.maxLength = "5000";
+      input.innerHTML = input.value = phases[phase];
+      input.className = 'textareas';
+      input.cols = "100";
+      input.rows = "40";
+      button.innerHTML = button.value = "Phase " + phase;
+      button.onclick = makePhaseClosure(input, phase);
+      div.appendChild(input); //appendChild
+      div.appendChild(button);
+    }
+}
+
+// This creates a properly bound closure for the onclick.
+function makePhaseClosure(input, phase) {
+    return function() {
+      handlePhaseButton(input, phase);
+    };
+}
+
+function handlePhaseButton(area, phase) {
+  // What to do?
+  // Count lines and expand the area.
+  var t = area.value;
+  var r = area.rows;
+  var fsize = area.fontsize;
+  var line_count = t.split('\n').length;
+  area.height = line_count * 20;
+  area.rows = line_count;
 }
